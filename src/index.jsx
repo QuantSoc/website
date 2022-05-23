acb71df30288dc168d30f16e19de758e7a997e93
imagine-hussain <hussain.nwz000@gmail.com>
2022-05-23 23:07:03 +1000

    fix index.jsx in root using index.css

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './index.less';
 
 ReactDOM.render(
   <React.StrictMode>
