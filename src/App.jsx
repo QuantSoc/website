b18b4eab6467d9adfb1f778432741056b0ea883c
imagine-hussain <hussain.nwz000@gmail.com>
2022-06-25 22:30:42 +1000

    route skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.scss';
 import './global.scss';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      This is empty as of now
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route ddath="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/events" element={<h1>events</h1>} />
+        <Route path="/resources" element={<h1>resources</h1>} />
+        <Route path="/contact" element={<h1>contact</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
