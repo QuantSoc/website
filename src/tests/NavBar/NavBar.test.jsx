61ae6b02c0073e22c4587af50467a64fd067661d
imagine-hussain <hussain.nwz000@gmail.com>
2022-06-25 21:47:18 +1000

    remove navbar links

--- src/tests/NavBar/NavBar.test.jsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import NavBar from '/src/components/common/NavBar';
-
-test('NavBar placeholder', () => {
-  render(<NavBar />);
-  expect(false).toBeFalsy();
-});
