a17774dde233cac12b2086857fdacb19c27c8f38
ollibowers <80164276+ollibowers@users.noreply.github.com>
2022-07-04 04:47:50 +1000

    fix: moved BrowserRouter to pass tests

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './global.scss';
 import {
   Routes,
   Route,
+  BrowserRouter,
 } from 'react-router-dom';
 
 import NotFoundPage from 'routes/NotFound';
@@ -12,16 +13,18 @@ import NavBar from 'components/common/NavBar';
 const App = () => {
   return (
     <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<NotFoundPage />} />
-        <Route path="/events" element={<NotFoundPage />} />
-        <Route path="/resources" element={<NotFoundPage />} />
-        <Route path="/sponsors" element={<NotFoundPage />} />
-        <Route path="/contact" element={<NotFoundPage />} />
-      </Routes>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<NotFoundPage />} />
+          <Route path="/events" element={<NotFoundPage />} />
+          <Route path="/resources" element={<NotFoundPage />} />
+          <Route path="/sponsors" element={<NotFoundPage />} />
+          <Route path="/contact" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.min.css';
@@ -8,9 +7,7 @@ import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
