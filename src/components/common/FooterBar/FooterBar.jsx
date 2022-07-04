92794425228f36393c553b137e4d2890bac4653b
imagine-hussain <hussain.nwz000@gmail.com>
2022-07-05 01:25:08 +1000

    add hor-bar for footer

--- src/components/common/FooterBar/FooterBar.jsx
+++ src/components/common/FooterBar/FooterBar.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 
+import QuantSocLogo from 'components/common/QuantSocLogo';
 import { Typography } from 'antd';
 
 import './index.scss';
@@ -10,20 +11,30 @@ const FooterBar = () => {
 
   return (
     <footer className="glb-foot">
-      <FooterColumn title="Contact us for Enquiries">
-        <li>email: quantsoc.org.au</li>
-      </FooterColumn>
-      <FooterColumn title="Links">
-        <li>Home</li>
-        <li>Resources</li>
-        <li>About Our Sponsors</li>
-      </FooterColumn>
-      <FooterColumn title="Our Social Media">
-        <li>Discord Community</li>
-        <li>Facebook Page</li>
-        <li>Facebook Group</li>
-        <li>LinkedIn</li>
-      </FooterColumn>
+      <HorizontalEOLBAR>
+        <LogoWithText />
+      </HorizontalEOLBAR>
+      <div className="glb-foot-content-cont">
+        <FooterColumn title="Contact us for Enquiries">
+          <li>email: quantsoc.org.au</li>
+        </FooterColumn>
+        <FooterColumn title="Links">
+          <li>Home</li>
+          <li>Resources</li>
+          <li>About Our Sponsors</li>
+          <li>Github</li>
+        </FooterColumn>
+        <FooterColumn title="Our Social Media">
+          <li>Discord Community</li>
+          <li>Facebook Page</li>
+          <li>Facebook Group</li>
+          <li>LinkedIn</li>
+        </FooterColumn>
+      </div>
+       {/* TODO: Add actualy C character */}
+      <HorizontalEOLBAR>
+        (c) 2022 QuantSoc UNSW
+      </HorizontalEOLBAR>
     </footer>
   );
 };
@@ -41,5 +52,20 @@ const FooterColumn = ({ title, children }) => {
     </div>
   )
 }
+
+// Should really be its own component - copied from navbar
+const LogoWithText = () => (
+  <div className="badge">
+    <QuantSocLogo type="circle" style={{ width: '3rem', height: '3rem' }} />
+    <div className="title">uantsoc</div>
+  </div>
+);
+
+const HorizontalEOLBAR = ({children}) => (
+  <div className="horizontal-eol-bar-cont">
+    { children }
+    <div className="horizontal-eol-bar"/>
+  </div>
+)
 
 export default FooterBar;
