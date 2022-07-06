748e7d34918ef741f790b64d1d9dc2bc84c3d166
WANNING <you@example.com>
2022-07-06 16:40:39 +1000

    resized purple background

--- src/routes/ResourcesSection/ResourceSection.jsx
+++ src/routes/ResourcesSection/ResourceSection.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './index.scss';
 
 const ResourceSection = () => {
   return (
-    <div>ResourceSection</div>
-  )
-}
-
-export default ResourceSection
+    <div className="ResourceSection">ResourceSection</div>
+  );
+};
+export default ResourceSection;
