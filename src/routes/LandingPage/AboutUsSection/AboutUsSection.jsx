b2cdb856bf8f8ff84b6b9f0a89c0ffd2f3394077
imagine-hussain <hussain.nwz000@gmail.com>
2022-07-13 16:39:15 +1000

    svg

--- src/routes/LandingPage/AboutUsSection/AboutUsSection.jsx
+++ src/routes/LandingPage/AboutUsSection/AboutUsSection.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
 import { Typography } from 'antd';
+import AboutUsImg from './AboutUsImg.svg';
 
 const AboutUsSection = () => {
   const { Title } = Typography;
+
+  const aboutUsTextBase = 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.';
+  const aboutUsText = aboutUsTextBase + aboutUsTextBase + aboutUsTextBase;
 
   return (
     <section className="about-us-section">
       <div className="about-us-cont">
         <div className="about-us-title">
           <Title level={2}>About Us</Title>
+        </div>
+        <div className="about-us-content-cont">
+          <div className="about-us-text-cont">
+            <p className="about-us-text">{aboutUsText}</p>
+          </div>
+          <div className="about-us-img">
+            <AboutUsImg />
+          </div>
         </div>
       </div>
     </section>
