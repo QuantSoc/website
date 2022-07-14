c121a1a5d90dfe916f89665224e222bc04cd6f6a
wanning <z5371162@unsw.edu.au>
2022-07-14 22:41:42 +1000

    fixed exports from component folder

--- src/routes/ResourceSection/ResourceSectionComponents/ResourcesBlurb.jsx
+++ src/routes/ResourceSection/ResourceSectionComponents/ResourcesBlurb.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ResourcesBlurb = () => {
   return (
-    <div className={styles.ResourcesBlurb}>See our resources from previous events and more!</div>
+    <div className="ResourcesBlurb">See our resources from previous events and more!</div>
   );
 };
 
