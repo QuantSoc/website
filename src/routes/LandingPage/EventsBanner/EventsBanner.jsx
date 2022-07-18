dd4f5415169cdcb084a84d974e2ca9a42ae1a3ed
Eric Lin <eric.lin6.2020@gmail.com>
2022-07-18 20:00:01 +1000

    Enabled eslint, changed Event banner text to 2 rem

--- src/routes/LandingPage/EventsBanner/EventsBanner.jsx
+++ src/routes/LandingPage/EventsBanner/EventsBanner.jsx
@@ -1,16 +1,15 @@
-/* eslint-disable */
-
 import React from 'react';
 import './index.scss';
-import classnames from 'classnames'
+import classnames from 'classnames';
 
 const EventsBanner = () => {
   return (
-    <div className='evts-banner'>
-      <EventsDivider highlight='1' />
-      <EventsDescription text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'/>
-      <EventsDivider highlight='6' />
+    <div className="evts-banner">
+      <EventsDivider highlight="1" />
+      <EventsDescription text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      />
+      <EventsDivider highlight="6" />
     </div>
   );
 };
@@ -18,26 +17,25 @@ const EventsBanner = () => {
 
 const EventsDescription = ({ text }) => {
   return (
-    <div className='evts-desc'>
+    <div className="evts-desc">
       <p1>
         {text}
       </p1>
     </div>
-  )
-}
+  );
+};
 
 const EventsDivider = ({ highlight, rows }) => {
-  var words = [];
-  for (var i = 0; i < rows; i++) {
-    if (i === parseInt(highlight)) {
-      words.push(<EventsIndividual type='solid' />);
+  const words = [];
+  for (let i = 0; i < rows; i += 1) {
+    if (i === parseInt(highlight, 10)) {
+      words.push(<EventsIndividual type="solid" />);
     } else {
       words.push(<EventsIndividual />);
     }
   }
-
   return (
-    <div className='evts-div'>
+    <div className="evts-div">
       {words}
     </div>
   );
@@ -45,20 +43,20 @@ const EventsDivider = ({ highlight, rows }) => {
 
 EventsDivider.defaultProps = {
   highlight: '0',
-  rows: '9'
-}
+  rows: '9',
+};
 
 const EventsIndividual = ({ type, text }) => {
   return (
     <div className={classnames('evts-single', type)}>
       {text}
-    </div >
+    </div>
   );
 };
 
 EventsIndividual.defaultProps = {
   type: 'trans',
-  text: 'events'
-}
+  text: 'events',
+};
 
 export default EventsBanner;
