98cdf5ea1419f39a08306f4a958f3268f8e5c0e5
Eric <eric.lin6.2020@gmail.com>
2022-09-01 16:33:30 +1000

    touched up on style

--- src/routes/LandingPage/AboutUsSection/AboutUsSection.jsx
+++ src/routes/LandingPage/AboutUsSection/AboutUsSection.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import './index.scss';
 
-import placeholder from '../../../assets/about_us_placeholder.png';
-
 import { Typography } from 'antd';
+import placeholder from '../../../assets/about_us_placeholder.png';
 
 const AboutUsSection = () => {
   const { Title } = Typography;
@@ -13,29 +12,29 @@ const AboutUsSection = () => {
 
   return (
     <div className="about-us-section">
-        <div className="about-us-cont">
-          <div className="about-us-text-cont">
-            <Title level={2} className="about-us-title">ABOUT US</Title>
-            <p className="about-us-text">{aboutUsText}</p>
-            <p className="about-us-text">{aboutUsText}</p>
-          </div>
-          <AboutUsImg imageSrc={placeholder} altText="Circle of mathematical symbols" />
+      <div className="about-us-cont">
+        <div className="about-us-text-cont">
+          <Title level={2} className="about-us-title">ABOUT US</Title>
+          <p className="about-us-text">{aboutUsText}</p>
+          <p className="about-us-text">{aboutUsText}</p>
         </div>
+        <AboutUsImg imageSrc={placeholder} altText="Circle of mathematical symbols" />
+      </div>
 
     </div>
   );
 };
 
-
-const AboutUsImg = ({imageSrc, altText}) => {
+const AboutUsImg = ({ imageSrc, altText }) => {
   return (
-    <div className='about-us-img-cont'>
-      <img className="about-us-img"
+    <div className="about-us-img-cont">
+      <img
+        className="about-us-img"
         src={imageSrc}
         alt={altText}
       />
     </div>
   );
-}
+};
 
 export default AboutUsSection;
