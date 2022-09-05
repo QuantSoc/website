cf39e345cb64f6386ec848076810ce640d67becc
imagine-hussain <hussain.nwz000@gmail.com>
2022-09-06 00:54:30 +1000

    less fiels

--- src/routes/LandingPage/SplashSection/SplashSection.jsx
+++ src/routes/LandingPage/SplashSection/SplashSection.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Button, Typography } from 'antd';
 import 'styles/index.scss';
 
 import QuantSocLogo from 'assets/crop_logo_circle.png';
 import mobileQuantSocLogo from 'assets/logo_circle.png';
-
-import SplashHeader from './SplashSectionComponents/SplashHeader';
-import SplashBlurb from './SplashSectionComponents/SplashBlurb';
-import SplashButton from './SplashSectionComponents/SplashButton';
 
 const SplashSection = () => {
   return (
     <div className="splash-section">
       <SplashIcon />
       <SplashLayout />
+    </div>
+  );
+};
+
+const SplashHeader = () => {
+  return (
+    <div className="splash-header">
+      <div className="unsw">UNSW</div>
+      <div>QUANT</div>
+      <div>SOC</div>
     </div>
   );
 };
@@ -35,5 +42,41 @@ const SplashIcon = () => {
     </picture>
   );
 };
+
+const SplashBlurb = () => {
+  return (
+    <div className="splash-blurb">
+      Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua.
+    </div>
+  );
+};
+
+const { Text } = Typography;
+
+const ButtonStyle = {
+  marginTop: '35px',
+  fontSize: '1.3em',
+  aspectratio: '1.51',
+  background: '#8234DA',
+  borderColor: '#8234DA',
+  height: '54px',
+  width: '12ch',
+  // backgroundColor: '#8234DA',
+};
+
+// TODO: remove this component entirely
+const SplashButton = () => {
+  return (
+    <Button type="primary" size="large" style={ButtonStyle}>
+      <Text strong style={{ color: 'white' }}>
+        Join Us
+      </Text>
+    </Button>
+  );
+};
+
 
 export default SplashSection;
--- src/routes/LandingPage/SplashSection/SplashSectionComponents/SplashHeader.jsx
+++ src/routes/LandingPage/SplashSection/SplashSectionComponents/SplashHeader.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
 import '../index.scss';
 
-const SplashHeader = () => {
-  return (
-    <div className="splash-header">
-      <div className="unsw">UNSW</div>
-      <div>QUANT</div>
-      <div>SOC</div>
-    </div>
-  );
-};
 
 export default SplashHeader;
