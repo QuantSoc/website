ba503f6c5c24aa322542c607e5088f62d2f8fc4b
imagine-hussain <hussain.nwz000@gmail.com>
2022-09-06 23:34:06 +1000

    fix footer margin + new stub for landing

--- src/routes/LandingPage/SplashSection/SplashSection.jsx
+++ src/routes/LandingPage/SplashSection/SplashSection.jsx
@@ -4,16 +4,26 @@ import 'styles/index.scss';
 
 import QuantSocLogo from 'assets/crop_logo_circle.png';
 import mobileQuantSocLogo from 'assets/logo_circle.png';
+import './index.scss';
 
 const { Text, Title } = Typography;
 
 /* const blurbText = '' */
 
+const blurbText = 'To be, or not to be, that is a question: Whether it is nobler in the mind to suffer. The slings and arrows of outrageous fortune Or to take arms against a sea of troubles, And by opposing end them? To die: to sleep; No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to, \'tis a consummation Devoutly to be wish\'d. To die, to sleep To sleep- perchance to dream: ay, there\'s the rub! For in that sleep of death what dreams may come When we have shuffled off this mortal coil, Must give us pause. There \'s the respect That makes calamity of so long life--William Shakespeare';
+
 const SplashSection = () => {
   return (
     <div className="splash-section">
+      <img src={QuantSocLogo} alt="QuantSoc Logo" className="quantsoc-logo"/>
       {/* The thing on the right */}
-      <div>
+      <div className="splash-cont">
+        <Title>
+          UNSW QUANT SOC
+        </Title>
+        <Text>
+          { blurbText }
+        </Text>
       </div>
     </div>
   );
