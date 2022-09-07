f1cefca843ae126e15b8aaba515f608c58552ae2
imagine-hussain <hussain.nwz000@gmail.com>
2022-09-08 01:36:16 +1000

    responsive added to resources sectin

--- src/routes/ResourceSection/ResourceSection.jsx
+++ src/routes/ResourceSection/ResourceSection.jsx
@@ -1,18 +1,86 @@
 import React from 'react';
 import { Button } from 'antd';
+import { RightOutlined } from '@ant-design/icons';
+import image from 'assets/comingsoon.png';
 // TODO: go absolute
+//
+//
 import useMediaQuery from '../../hooks/useMediaQuery';
 import { ResourcesDivider } from './ResourceSectionComponents';
-import ResourceBlock from './ResourceSectionComponents/ResourceBlock';
 import './index.scss';
 
+
 const ButtonStyle = {
-  fontSize: '1.7em',
-  height: '67px',
+  fontSize: '1.2rem',
+  height: '2.7rem',
+  background: '#8234DA',
+  borderColor: '#8234DA',
+  borderRadius: '3px',
 };
 
 /* eslint-disable */
+
+const resourcesBlurbText = `See our resources from previous events and more!`;
+
+/* const ButtonStyle = { */
+/*   marginTop: '35px', */
+/*   fontSize: '1em', */
+/*   height: '67px', */
+/*   aspectratio: '1.51', */
+/*   background: '#8234DA', */
+/*   borderColor: '#8234DA', */
+/* }; */
+
+
+
 const ResourceSection = () => {
+  return (
+    <div className="resources-section">
+      <div className="resources-header">
+        <div className="resources-header-cont">
+          <div className="resources-title">
+            Resources
+              <span className="resources-title-arrow">&gt; &gt; &gt;</span>
+          </div>
+          <div className="resources-blurb">{resourcesBlurbText}</div>
+        </div>
+        <Button
+          type="primary"
+          size="large"
+          style={ButtonStyle}
+        >
+          See More
+        </Button>
+      </div>
+      {/*  */}
+      <div className="resources-layout">
+        <ResourceBlock name="WORKSHOPS" />
+        <ResourceBlock name="ARTICLES" />
+        <ResourceBlock name="BLOGS" />
+      </div>
+    </div>
+  )
+};
+
+
+const ResourceBlock = ({ name }) => {
+  return (
+    <div className="resource-block">
+      <div>
+        <img className="image" src={image} alt="coming soon" />
+      </div>
+      <div>
+        <Button type="primary" size="large" style={ButtonStyle}>
+          {name}
+          <RightOutlined />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+
+const ResourceSectionAHH = () => {
   const isSmall = useMediaQuery('(max-width: 768px)');
 
   console.log('isSmall', isSmall);
@@ -20,8 +88,8 @@ const ResourceSection = () => {
   return !isSmall ? (
     <div className="ResourceSection">
       <div className="ResourcesHeader">
-        <PageTitle />
-        <div className="ResourcesBlurb">See our resources from previous events and more!</div>
+        <PageTitle isSmall={false}/>
+        <div className="resources-blurb">See our resources from previous events and more!</div>
         <div>
           <Button type="primary" size="large" style={ButtonStyle}>
             SEE MORE...
@@ -36,37 +104,31 @@ const ResourceSection = () => {
       </div>
     </div>
   ) : (
-    <div className="ResourceSection">
-      YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo
-      YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo
-      YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo
-      YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo
-      YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo
-      YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo
-      YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo
-      YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo
-    </div>
+    <ResourceSectionSmall />
   );
 };
 
-const ResourceSectionSmall = ({ isSmall }) => {
+const ResourceSectionSmall = () => {
   return (
-    <div className="ResourceSection">
-      <div className="ResourcesHeader">
-        <PageTitle isSmall={isSmall} />
-        <div className="ResourcesBlurb">See our resources from previous events and more!</div>
-        <div>
-          <Button type="primary" size="large" style={ButtonStyle}>
-            SEE MORE...
-          </Button>
-        </div>
-      </div>
-      <ResourcesDivider />
-      <div className="ResourcesLayout">
-        <ResourceBlock name="WORKSHOPS" />
-        <ResourceBlock name="ARTICLES" />
-        <ResourceBlock name="BLOGS" />
-      </div>
+    <div>
+      AHHHHHHHHhh
+      {/* <div className="ResourceSection"> */}
+      {/*   <div className="ResourcesHeader"> */}
+      {/*     <PageTitle isSmall={true} /> */}
+      {/*     <div className="resources-blurb">See our resources from previous events and more!</div> */}
+      {/*     <div> */}
+      {/*       <Button type="primary" size="large" style={ButtonStyle}> */}
+      {/*         SEE MORE... */}
+      {/*       </Button> */}
+      {/*     </div> */}
+      {/*   </div> */}
+      {/*   <ResourcesDivider /> */}
+      {/*   <div className="ResourcesLayout"> */}
+      {/*     <ResourceBlock name="WORKSHOPS" /> */}
+      {/*     <ResourceBlock name="ARTICLES" /> */}
+      {/*     <ResourceBlock name="BLOGS" /> */}
+      {/*   </div> */}
+      {/* </div> */}
     </div>
   );
 };
