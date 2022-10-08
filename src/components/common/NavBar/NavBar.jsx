3fdc840a2d643f37277912f220354ada2d404a8e
Giulia <z5361910@ad.unsw.edu.au>
2022-10-08 22:26:57 +1100

    hello

--- src/components/common/NavBar/NavBar.jsx
+++ src/components/common/NavBar/NavBar.jsx
@@ -9,6 +9,22 @@ const NavBar = () => {
         <QuantSocLogo type="circle" style={{ width: '3rem', height: '3rem' }} />
         <div className="title">uantsoc</div>
       </div>
+      <div className="menuTabs">  
+        <div className='menuWord'>
+          EVENTS
+        </div>
+        <div className='menuWord'>
+          RESOURCES
+        </div>
+        <div className='menuWord'>
+          ABOUT US
+        </div>
+        <div className='menuWord'>
+          SPONSORS
+        </div>
+        <text> ABOUT US</text>
+        CONTACT US
+      </div>
       <div className="links">
         {/* <Link></Link> */}
       </div>
