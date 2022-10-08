b09403275f4b560199569768073be3dc0cc8eab9
wanning <z5371162@unsw.edu.au>
2022-10-09 10:39:14 +1100

    about us section now leads to about us page on click

--- src/routes/LandingPage/AboutUsSection/AboutUsSection.jsx
+++ src/routes/LandingPage/AboutUsSection/AboutUsSection.jsx
@@ -3,36 +3,42 @@ import './index.less';
 
 import { Typography } from 'antd';
 import placeholder from '../../../assets/about_us_placeholder.png';
+import { Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const AboutUsSection = () => {
   const { Title } = Typography;
 
-  const aboutUsTextP1 = 'Our goal is to cultivate an in-person and online community of like-minded students by on providing careers events, educational workshops, interview problem-solving sessions and theoretical reading groups aimed specifically at STEM students exploring highly quantitative or technological career pathways in finance and operations research.';
-  const aboutUsTextP2 = 'What sets us apart from other UNSW student-run finance focussed societies?\nQuantitative finance is distinguished from other financial industries such as investment banking or equity by it\'s maths-heavy approach to financial theory, unique interview problems and specialised software engineering challenges. We\'re excited to be focusing in on the unique value that science, mathematics and computer science graduates can bring to private industry in the domains of financial engineering, stochastic time series analysis, mathematical optimisation, electronic trading infrastructure and statistical learning.';
+  const aboutUsTextP1 =
+    'Our goal is to cultivate an in-person and online community of like-minded students by on providing careers events, educational workshops, interview problem-solving sessions and theoretical reading groups aimed specifically at STEM students exploring highly quantitative or technological career pathways in finance and operations research.';
+  const aboutUsTextP2 =
+    "What sets us apart from other UNSW student-run finance focussed societies?\nQuantitative finance is distinguished from other financial industries such as investment banking or equity by it's maths-heavy approach to financial theory, unique interview problems and specialised software engineering challenges. We're excited to be focusing in on the unique value that science, mathematics and computer science graduates can bring to private industry in the domains of financial engineering, stochastic time series analysis, mathematical optimisation, electronic trading infrastructure and statistical learning.";
 
   return (
-    <div className="about-us-section">
-      <div className="about-us-cont">
-        <div className="about-us-text-cont">
-          <Title level={2} className="about-us-title">ABOUT US</Title>
-          <p className="about-us-text">{aboutUsTextP1}</p>
-          <p className="about-us-text">{aboutUsTextP2}</p>
+    <Link to='/about-us'>
+      <div className='about-us-section'>
+        <div className='about-us-cont'>
+          <div className='about-us-text-cont'>
+            <Title level={2} className='about-us-title'>
+              ABOUT US
+            </Title>
+            <p className='about-us-text'>{aboutUsTextP1}</p>
+            <p className='about-us-text'>{aboutUsTextP2}</p>
+          </div>
+          <AboutUsImg
+            imageSrc={placeholder}
+            altText='Circle of mathematical symbols'
+          />
         </div>
-        <AboutUsImg imageSrc={placeholder} altText="Circle of mathematical symbols" />
       </div>
-
-    </div>
+    </Link>
   );
 };
 
 const AboutUsImg = ({ imageSrc, altText }) => {
   return (
-    <div className="about-us-img-cont">
-      <img
-        className="about-us-img"
-        src={imageSrc}
-        alt={altText}
-      />
+    <div className='about-us-img-cont'>
+      <img className='about-us-img' src={imageSrc} alt={altText} />
     </div>
   );
 };
