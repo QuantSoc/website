f3aafe3813b1c44c22c3c4ece7aeec77c3926807
Eric <eric.lin6.2020@gmail.com>
2022-10-09 14:14:05 +1100

    Mobile friendly carousel mostly finished, need to keep image centered when overflow

--- src/routes/LandingPage/EventsPreviewSection/EventsCarousel/EventContent.jsx
+++ src/routes/LandingPage/EventsPreviewSection/EventsCarousel/EventContent.jsx
@@ -3,23 +3,43 @@ import './index.less';
 import { Typography } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
 import LandingButton from 'components/common/LandingButton/LandingButton';
+import useMediaQuery from 'hooks/useMediaQuery';
 
 const { Paragraph } = Typography;
-
-const ParaStyle = {
-  fontSize: '1.7em',
-  textAlign: 'left',
-  lineHeight: '31px',
-  marginBottom: '0px',
-};
 
 const EventContent = ({
   desc, date, time, delivery,
 }) => {
+  const isDesktop = useMediaQuery('(min-width: 860px)');
+
+  const ParaStyleDesktop = {
+    fontSize: '1.7em',
+    textAlign: 'left',
+    lineHeight: '31px',
+    marginBottom: '0px',
+  };
+
+  const ParaStyleMobile = {
+    fontSize: '1.4em',
+    textAlign: 'left',
+    lineHeight: '31px',
+    marginBottom: '0px',
+  };
+
+  const btnDesktop = {
+    marginTop: '35px',
+    height: '67px',
+  };
+
+  const btnMobile = {
+    marginTop: '25px',
+    height: '50px',
+  };
+
   return (
     <div className="carousel-content">
       <div className="event-description">
-        <Paragraph style={ParaStyle} ellipsis={{ rows: 6, expandable: false, symbol: 'more' }}>
+        <Paragraph style={isDesktop ? ParaStyleDesktop : ParaStyleMobile} ellipsis={{ rows: 6, expandable: false, symbol: 'more' }}>
           {desc}
         </Paragraph>
         <div className="events-info">
@@ -34,7 +54,7 @@ const EventContent = ({
           {' '}
           <br />
           {/* uhhh... button does nothing... will change soon */}
-          <LandingButton style={{ marginTop: '35px', height: '67px' }}>
+          <LandingButton style={isDesktop ? btnDesktop : btnMobile}>
             GO
             {' '}
             <RightOutlined />
