ed1a46e35b4e7f70447483894d625789b96d8bb3
imagine-hussain <hussain.nwz000@gmail.com>
2022-10-09 20:06:59 +1100

    mmmm ternary

--- src/components/common/NavBar/NavBar.jsx
+++ src/components/common/NavBar/NavBar.jsx
@@ -1,11 +1,11 @@
-import { Typography } from 'antd';
+/* import { Typography } from 'antd'; */
 import { MenuOutlined, CloseOutlined } from '@ant-design/icons';
 import QuantSocLogo from 'components/common/QuantSocLogo';
 import './index.less';
 import { useState } from 'react';
 import useMediaQuery from 'hooks/useMediaQuery';
 
-const { Text } = Typography;
+/* const { Text } = Typography; */
 
 // TODO: Wrapper around antd nav
 const NavBar = () => {
@@ -16,68 +16,86 @@ const NavBar = () => {
   }
   return (
     <div className="navbar">
-      {isOpen && (
-        <div className="navbar_normal">
-          <div className="badge">
-            <QuantSocLogo type="circle" style={{ width: '3rem', height: '3rem' }} />
-            <div className="title">uantsoc</div>
-          </div>
-          <div className="menuTabs">
-            <div className="menuWord">
-              EVENTS
-            </div>
-            <div className="menuWord">
-              RESOURCES
-            </div>
-            <div className="menuWord">
-              ABOUT US
-            </div>
-            <div className="menuWord">
-              SPONSORS
-            </div>
-            <div className="menuWord">
-              CONTACT US
-            </div>
-          </div>
-          <MenuOutlined className="dropDown" onClick={() => { return setOpen(!isOpen); }} />
-          <div className="links">
-            {}
-          </div>
-        </div>
-      ) }
+      {
+        isOpen
+          ? (
+            <NavBarOpen
+              setOpen={setOpen}
+              isOpen={isOpen}
+            />
+          ) : (
+            <NavBarClosed
+              setOpen={setOpen}
+              isOpen={isOpen}
+            />
+          )
+      }
+    </div>
+  );
+};
 
-      {!isOpen && (
-        <div className="navbar_toggled">
-          <div className="toggle_top">
-            <div className="badge">
-              <QuantSocLogo type="circle" style={{ width: '3rem', height: '3rem' }} />
-              <div className="title">uantsoc</div>
-            </div>
-            <CloseOutlined className="exit" onClick={() => { return setOpen(!isOpen); }} />
-          </div>
-          <div className="menuTabs">
-            <div className="menuWord">
-              EVENTS
-            </div>
-            <div className="menuWord">
-              RESOURCES
-            </div>
-            <div className="menuWord">
-              ABOUT US
-            </div>
-            <div className="menuWord">
-              SPONSORS
-            </div>
-            <div className="menuWord">
-              CONTACT US
-            </div>
-          </div>
-          <div className="links">
-            {}
-          </div>
+export const NavBarOpen = ({ isOpen, setIsOpen }) => {
+  return (
+    <div className="navbar_normal">
+      <div className="badge">
+        <QuantSocLogo type="circle" style={{ width: '3rem', height: '3rem' }} />
+        <div className="title">uantsoc</div>
+      </div>
+      <div className="menuTabs">
+        <div className="menuWord">
+          EVENTS
         </div>
-      )}
+        <div className="menuWord">
+          RESOURCES
+        </div>
+        <div className="menuWord">
+          ABOUT US
+        </div>
+        <div className="menuWord">
+          SPONSORS
+        </div>
+        <div className="menuWord">
+          CONTACT US
+        </div>
+      </div>
+      <MenuOutlined className="dropDown" onClick={() => { return setIsOpen(!isOpen); }} />
+      <div className="links">
+        {}
+      </div>
+    </div>
+  );
+};
 
+export const NavBarClosed = ({ isOpen, setIsOpen }) => {
+  return (
+    <div className="navbar_toggled">
+      <div className="toggle_top">
+        <div className="badge">
+          <QuantSocLogo type="circle" style={{ width: '3rem', height: '3rem' }} />
+          <div className="title">uantsoc</div>
+        </div>
+        <CloseOutlined className="exit" onClick={() => { return setIsOpen(!isOpen); }} />
+      </div>
+      <div className="menuTabs">
+        <div className="menuWord">
+          EVENTS
+        </div>
+        <div className="menuWord">
+          RESOURCES
+        </div>
+        <div className="menuWord">
+          ABOUT US
+        </div>
+        <div className="menuWord">
+          SPONSORS
+        </div>
+        <div className="menuWord">
+          CONTACT US
+        </div>
+      </div>
+      <div className="links">
+        {}
+      </div>
     </div>
   );
 };
