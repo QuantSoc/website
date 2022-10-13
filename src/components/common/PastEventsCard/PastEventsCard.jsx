0518d5ce25213665afeae0def858aa726ae88c1e
imagine-hussain <hussain.nwz000@gmail.com>
2022-10-13 23:26:59 +1100

    eslint'

--- src/components/common/PastEventsCard/PastEventsCard.jsx
+++ src/components/common/PastEventsCard/PastEventsCard.jsx
@@ -5,22 +5,23 @@ import './index.less';
 
 const { Meta } = Card;
 
-const PastEventsCard = ({ img, title, description, actions,  width }) => {
-
+const PastEventsCard = ({
+  img, title, description, actions, width,
+}) => {
   return (
     <Card
       className="past-events-card"
       hoverable
       style={{ width: width ?? 300, margin: '20px' }}
-      cover={<img alt={title} src={img} style={{padding: 20}}/>}
-      actions={actions ? actions : [
+      cover={<img alt={title} src={img} style={{ padding: 20 }} />}
+      actions={actions || [
         <SettingOutlined key="setting" />, // Facebook or smth
         <div>
           Read More
           &nbsp;
           <RightOutlined />
           {/* <EditOutlined key="edit" /> */}
-        </div>
+        </div>,
       ]}
     >
       <Meta
