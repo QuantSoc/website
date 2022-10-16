6f4c7e3c36047f66373afb4e5e350832c1e5d48a
imagine-hussain <hussain.nwz000@gmail.com>
2022-10-16 19:12:06 +1100

    change icons + stub modal

--- src/components/common/InfoCard/InfoCard.jsx
+++ src/components/common/InfoCard/InfoCard.jsx
@@ -1,13 +1,49 @@
+/* eslint-disable */
 import React, { useState } from 'react';
-import { Card, Modal } from 'antd';
-import { RightOutlined, SettingOutlined, CalendarOutlined } from '@ant-design/icons';
+import { Card, Modal, Button } from 'antd';
+import {
+  RightOutlined, SettingOutlined, CalendarOutlined, ArrowsAltOutlined, EditOutlined, FacebookOutlined,
+} from '@ant-design/icons';
 import './index.less';
 
 const { Meta } = Card;
 
+const CalendarConfirmModal = ({ open, setOpen }) => {
+  const handleOk = () => {
+    setOpen(false);
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Modal
+      title="Example Event"
+      open={open}
+      onOk={handleOk}
+      onCancel={handleCancel}
+      footer={[
+        <Button key="close" onClick={handleCancel} />,
+        <Button key="submit" type="primary" onClick={handleOk} />,
+      ]}
+    >
+      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+    </Modal>
+  );
+};
+
 const InfoCard = ({
   img, title, description, actions, width,
 }) => {
+
+  const [calendarOpen, setCalendarOpen] = useState(false);
+
+  const openCalendar = () => {
+    // works but hidden on dev
+    /* setCalendarOpen(true); */
+  }
+
   return (
     <Card
       className="past-events-card"
@@ -16,25 +52,29 @@ const InfoCard = ({
       cover={<img alt={title} src={img} style={{ padding: 20 }} />}
       actions={actions || [
         <a href="https://www.facebook.com/unswquantsoc" target="_blank" rel="noreferrer">
-          <SettingOutlined key="setting" />
-        </a>, // Facebook or smth
+          <FacebookOutlined key="facebook" />
+        </a>, // Facebook
+        <CalendarOutlined />,
         <div>
-          Read More
-          &nbsp;
-          <RightOutlined />
-          <Modal title={title}>
-            <img alt={title} src={img} style={{ maxWidth: 460, padding: 20 }} />
-            <p>{description}</p>
-          </Modal>
+          <ArrowsAltOutlined
+            onClick={openCalendar}
+          />
           {/* <EditOutlined key="edit" /> */}
         </div>,
-        <CalendarOutlined />,
       ]}
     >
       <Meta
         className="card-cont"
         title={title}
         description={description}
+      />
+      {/* <Modal title={title}> */}
+      {/*   <img alt={title} src={img} style={{ maxWidth: 460, padding: 20 }} /> */}
+      {/*   <p>{description}</p> */}
+      {/* </Modal> */}
+      <CalendarConfirmModal
+        open={calendarOpen}
+        setOpen={setCalendarOpen}
       />
     </Card>
   );
