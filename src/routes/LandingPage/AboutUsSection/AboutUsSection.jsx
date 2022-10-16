e76dfb9c10fdbe206e55bbac049fc39baf8d537d
wanning <z5371162@unsw.edu.au>
2022-10-16 19:51:47 +1100

    done porting about

--- src/routes/LandingPage/AboutUsSection/AboutUsSection.jsx
+++ src/routes/LandingPage/AboutUsSection/AboutUsSection.jsx
@@ -15,7 +15,7 @@ const AboutUsSection = () => {
     "What sets us apart from other UNSW student-run finance focussed societies?\nQuantitative finance is distinguished from other financial industries such as investment banking or equity by it's maths-heavy approach to financial theory, unique interview problems and specialised software engineering challenges. We're excited to be focusing in on the unique value that science, mathematics and computer science graduates can bring to private industry in the domains of financial engineering, stochastic time series analysis, mathematical optimisation, electronic trading infrastructure and statistical learning.";
 
   return (
-    <Link to='/about-us'>
+    <Link to='/about'>
       <div className='about-us-section'>
         <div className='about-us-cont'>
           <div className='about-us-text-cont'>
