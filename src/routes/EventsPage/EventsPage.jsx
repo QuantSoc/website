03cbf612efe554b1f93fc6ff0e8a8a35b8236555
Shmu9 <sam.axford02@gmail.com>
2023-08-10 15:26:21 +1000

    chore: add real example event InfoCard to EventsPage.

--- src/routes/EventsPage/EventsPage.jsx
+++ src/routes/EventsPage/EventsPage.jsx
@@ -3,11 +3,23 @@ import InfoCard from 'components/common/InfoCard';
 
 /* import logo from 'assets/logo_circle.png'; */
 import logo from 'assets/SponsorImages/citsec.png';
-
+import banner from 'assets/banner_revisionseminar_13.8.23.png';
+// import banner from 'assets/SponsorImages/unsw.png';
 
 const EventsPage = () => {
   return (
     <div>
+      <InfoCard
+        title="MathSoc x QuantSoc's 3311 Revision Seminar"
+        description="Fourier series got you feeling frightened? PDE's causing panic?
+                     Struggle no more, as the MATH3311 revision seminar is here to help!"
+        link="https://fb.me/e/1hEXVg8M5"
+        img={banner}
+        time="SUNDAY 13 AUGUST 2023 FROM 12:00-14:00"
+        location="YouTube Livestream"
+        duration="2 hr"
+        people="Students Only"
+      />
       <InfoCard
         title="Quantitative Society"
         description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
