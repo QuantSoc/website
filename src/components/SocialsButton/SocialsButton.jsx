a812ad4b517768ca5c8862d20990a39afe96945e
Alex Xu <xulex6@gmail.com>
2023-11-08 11:31:34 +0000

    feat: improve button style and html conventions

--- src/components/SocialsButton/SocialsButton.jsx
+++ src/components/SocialsButton/SocialsButton.jsx
@@ -1,19 +1,18 @@
 import './index.less';
 
-const SocialsButton = ({ btnText, sociallink, IconComponent, iconStyle }) => {
-    const navigateToLink = () => {
-      window.open(sociallink, '_blank');
-    };
-  
-    return (
-      <button className="socials-button-container" type="button" onClick={navigateToLink}>
-        <IconComponent style={iconStyle} className="socials-button-icon"/>
-        <div className="socials-button-text">{btnText}</div>
-      </button>
-    );
-  };
-  
+const SocialsButton = ({ btnText, socialsLink, IconComponent }) => {
+  return (
+    <button
+      className="socials-button-container"
+      type="button"
+      onClick={() => {
+        window.open(socialsLink, '_blank').focus();
+      }}
+    >
+      <IconComponent className="socials-button-icon" />
+      <div className="socials-button-text">{btnText}</div>
+    </button>
+  );
+};
 
 export default SocialsButton;
-  
-  
