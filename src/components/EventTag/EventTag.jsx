45d891beed5c8c09acc9cb464481efc83721e71f
Alex Xu <xulex6@gmail.com>
2023-11-08 11:06:28 +0000

    feat: add EventTag component with icons and text

---
+++ src/components/EventTag/EventTag.jsx
@@ -0,0 +1,25 @@
+import { BiCandles, BiParty, BiBriefcase } from 'react-icons/bi';
+import { LiaChalkboardTeacherSolid } from 'react-icons/lia';
+import './index.less';
+
+const iconMap = {
+  trading: <BiCandles className="event-tag__icon" />,
+  fun: <BiParty className="event-tag__icon" />,
+  career: <BiBriefcase className="event-tag__icon" />,
+  workshop: <LiaChalkboardTeacherSolid className="event-tag__icon" />,
+};
+const tagMap = {
+  trading: 'Mock Trading',
+  fun: 'Fun & Games',
+  career: 'Careers',
+  workshop: 'Workshop',
+};
+const EventTag = ({ tagType }) => {
+  return (
+    <div className="event-tag">
+      {iconMap[tagType]}
+      <p className="event-tag__text">{tagMap[tagType]}</p>
+    </div>
+  );
+};
+export default EventTag;
