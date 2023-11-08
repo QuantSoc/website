44836df9ae462ffa0b41aa752d1cbc6bd86e4057
Alex Xu <xulex6@gmail.com>
2023-11-08 11:31:59 +0000

    feat: improve button spacing on hero section

--- src/routes/EventsPage/HeroSection/HeroSection.jsx
+++ src/routes/EventsPage/HeroSection/HeroSection.jsx
@@ -1,9 +1,6 @@
+import { BsFacebook, BsDiscord } from 'react-icons/bs';
 import SocialsButton from 'components/SocialsButton/SocialsButton';
 import eventGathering from '../../../assets/event-gathering.svg';
-import {
-  BsFacebook,
-  BsDiscord,
-} from 'react-icons/bs';
 import './index.less';
 
 const HeroSection = () => {
@@ -21,22 +18,27 @@ const HeroSection = () => {
         <div className="hero-item stack">
           <h1 className="hero-heading">Events</h1>
           <p className="text-body">
-          We run a wide-variety of events for fun, learning new skills and careers. 
-          Keep up to date with the latest workshops, competitions, games nights, company meet-and-greets, 
-          and more events via Facebook or Discord!
+            We run a wide-variety of events for fun, learning new skills and
+            careers. Keep up to date with the latest workshops, competitions,
+            games nights, company meet-and-greets, and more events via Facebook
+            or Discord!
           </p>
 
-          <div style={{ display: 'flex', gap: '10px' }}>
-            <SocialsButton iconStyle={{ width: '25px', height: '25px', marginLeft: '6px' }} btnText="Facebook" sociallink="https://www.facebook.com/unswquantsoc/" IconComponent={BsFacebook} />
-            <SocialsButton iconStyle={{ width: '25px', height: '24px', marginLeft: '6px' }} btnText="Discord" sociallink="https://discord.gg/2qYRNKubRz" IconComponent={BsDiscord} />
+          <div className="socials-buttons">
+            <SocialsButton
+              btnText="Facebook"
+              socialsLink="https://www.facebook.com/unswquantsoc/"
+              IconComponent={BsFacebook}
+            />
+            <SocialsButton
+              btnText="Discord"
+              socialsLink="https://discord.gg/2qYRNKubRz"
+              IconComponent={BsDiscord}
+            />
           </div>
         </div>
-
       </section>
     </div>
   );
 };
 export default HeroSection;
-
-
-
