77e9f2e7fe356fb26ea43f1798437c31fa97231a
Alex Xu <xulex6@gmail.com>
2023-11-30 06:19:06 +0000

    feat: move resource outline into its own component

--- src/routes/LandingPage/LandingPage.jsx
+++ src/routes/LandingPage/LandingPage.jsx
@@ -1,3 +1,5 @@
+import mockTradingScreenshot from 'assets/quantsoc-graphics/mock-trading-screenshot.svg';
+import ResourceOutline from 'components/ResourceOutline';
 import EventsSection from './EventsSection/EventsSection';
 import HeroSection from './HeroSection/HeroSection';
 import SponsorshipSection from '../../components/SponsorshipSection';
@@ -6,6 +8,21 @@ const LandingPage = () => {
   return (
     <div className="page">
       <HeroSection />
+      <ResourceOutline
+        outlineTitle="Mock Trading Game"
+        index={0}
+        imgSrc={mockTradingScreenshot}
+        redirectLink="https://mtg.quantsoc.org"
+        redirectBtnText="Play"
+      >
+        <p>
+          Dive into the fast-paced world of trading, hone your investment
+          strategies, and compete with fellow traders in a mock environment.
+          Create your own markets and play amongst your friends.
+        </p>
+        <br />
+        <p>Learn, compete and have fun in QuantSoc&apos;s Mock Trading Game!</p>
+      </ResourceOutline>
       <EventsSection />
       <SponsorshipSection />
     </div>
--- src/routes/ResourcesPage/ResourcesPage.jsx
+++ src/routes/ResourcesPage/ResourcesPage.jsx
@@ -1,6 +1,6 @@
 import mockTradingScreenshot from 'assets/quantsoc-graphics/mock-trading-screenshot.svg';
+import ResourceOutline from 'components/ResourceOutline';
 import HeroSection from './HeroSection/HeroSection';
-import ResourceOutline from './ResourceOutline/ResourceOutline';
 import WorkshopSection from './WorkshopSection/WorkshopSection';
 import OpportunitiesSection from './OpportunitiesSection/OpportunitiesSection';
 
@@ -14,7 +14,15 @@ const ResourcesPage = () => {
         imgSrc={mockTradingScreenshot}
         redirectLink="https://mtg.quantsoc.org"
         redirectBtnText="Play"
-      />
+      >
+        <p>
+          Dive into the fast-paced world of trading, hone your investment
+          strategies, and compete with fellow traders in a mock environment.
+          Create your own markets and play amongst your friends.
+        </p>
+        <br />
+        <p>Learn, compete and have fun in QuantSoc&apos;s Mock Trading Game!</p>
+      </ResourceOutline>
       <WorkshopSection />
       <OpportunitiesSection />
     </div>
