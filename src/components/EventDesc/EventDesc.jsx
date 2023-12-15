bb36bed641584b86893dffec3fd3a6c591bf7935
Alex Xu <xulex6@gmail.com>
2023-12-15 06:04:58 +0000

    feat: add an intuitive date comprehension function

--- src/components/EventDesc/EventDesc.jsx
+++ src/components/EventDesc/EventDesc.jsx
@@ -3,14 +3,43 @@ import { BiCalendarAlt, BiTimeFive, BiMap } from 'react-icons/bi';
 import { AiOutlineUsergroupAdd } from 'react-icons/ai';
 import { FiLink } from 'react-icons/fi';
 
-const EventDesc = ({ location, sublocation, date, times, cohosts, link }) => {
+const intuitiveDates = (date, endDate) => {
+  const start = new Date(date);
+  const end = new Date(endDate);
+  const startMonth = start.toLocaleString('default', { month: 'short' });
+  const endMonth = end.toLocaleString('default', { month: 'short' });
+  const startYear = start.getFullYear();
+  const endYear = end.getFullYear();
+  const startDay = start.getDate();
+  const endDay = end.getDate();
   const dateStr = new Date(date).toLocaleDateString('en-GB', {
     weekday: 'short',
     day: 'numeric',
     month: 'short',
     year: 'numeric',
   });
+  if (date && !endDate) return dateStr;
+  if (!date && !endDate) return 'N/A';
+  if (startDay === endDay) return dateStr;
+  if (startMonth === endMonth && startYear === endYear) {
+    return `${startDay} - ${endDay}, ${startMonth} ${startYear}`;
+  }
+  if (startMonth !== endMonth && startYear === endYear) {
+    return `${startDay} ${startMonth} - ${endDay} ${endMonth}, ${startYear}`;
+  }
+  return `${startDay} ${startMonth} ${startYear} - ${endDay} ${endMonth} ${endYear}`;
+};
 
+const EventDesc = ({
+  location,
+  sublocation,
+  date,
+  endDate,
+  times,
+  cohosts,
+  link,
+}) => {
+  const dateStr = intuitiveDates(date, endDate);
   const daysLeft = (new Date(date) - new Date()) / (1000 * 3600 * 24);
 
   return (
@@ -25,7 +54,7 @@ const EventDesc = ({ location, sublocation, date, times, cohosts, link }) => {
         }`}
       >
         <BiCalendarAlt className="icon" />
-        <p>{date ? dateStr : 'N/A'}</p>
+        <p>{dateStr}</p>
       </div>
       <div className="event-desc time">
         <BiTimeFive className="icon" />
