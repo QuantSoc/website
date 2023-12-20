f2a49ce0de7a048be3199032d7244ebaf40e8afe
Shmu9 <sam.axford02@gmail.com>
2023-12-20 14:57:52 +1100

    feat: add anchoring functionality to resource page sections.

--- src/routes/ResourcesPage/WorkshopSection/WorkshopSection.jsx
+++ src/routes/ResourcesPage/WorkshopSection/WorkshopSection.jsx
@@ -1,8 +1,8 @@
 import EventsCarousel from 'components/EventsCarousel';
 
-const WorkshopSection = () => {
+const WorkshopSection = ({ anchorTag }) => {
   return (
-    <section className="events-section">
+    <section id={anchorTag} className="events-section">
       <h1 className="events-section__header">Workshops</h1>
       <p>Explore our workshops designed to refine your skills!</p>
       <EventsCarousel
