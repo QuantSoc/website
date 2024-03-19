9a5ed5ad0846a78635a429bd9b1f9ceb985960a0
Shmu9 <sam.axford02@gmail.com>
2024-03-19 18:50:55 +1100

    refactor: disable eslint preference for prefer-default-export.

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+// import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,5 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+// eslint-disable-next-line import/prefer-default-export
 export const db = getFirestore();
