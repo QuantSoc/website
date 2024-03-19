8e442489665c6477774fbece339565f4e157a420
Shmu9 <sam.axford02@gmail.com>
2024-03-19 18:33:00 +1100

    feature: remove carousel features from event preview on creation page.

--- src/components/EventPreview/EventPreview.jsx
+++ src/components/EventPreview/EventPreview.jsx
@@ -1,96 +1,46 @@
 import './index.less';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import useModal from 'hooks/useModal';
 
-import Carousel from 'components/Carousel/Carousel';
 import EventCard from 'components/EventCard';
 import EventModal from 'components/EventModal';
 
-
-const setInitialStep = () => {
-  if (window.innerWidth <= 950) return 1;
-  if (window.innerWidth <= 1400) return 2;
-  return 3;
-};
-
-const EventsCarousel = ({ formData }) => {
-  const events = [formData]
-
-  const [eventIndex, setEventIndex] = useState(0);
-  const [step, setStep] = useState(setInitialStep());
+const EventsPreview = ({ formData }) => {
   const { isOpen, toggleModal } = useModal();
 
-  useEffect(() => {
-    const mediumQuery = window.matchMedia('(max-width: 1400px)');
-    const smallQuery = window.matchMedia('(max-width: 950px)');
-
-    const handleMediumQueryChange = (event) => {
-      if (event.matches) setStep(2);
-      if (!event.matches) setStep(3);
-    };
-    const handleSmallQueryChange = (event) => {
-      if (event.matches) setStep(1);
-      if (!event.matches) setStep(2);
-    };
-
-    mediumQuery.addEventListener('change', handleMediumQueryChange);
-    smallQuery.addEventListener('change', handleSmallQueryChange);
-    return () => {
-      mediumQuery.removeEventListener('change', handleMediumQueryChange);
-      smallQuery.removeEventListener('change', handleSmallQueryChange);
-    };
-  }, []);
-
-  const createEventSlides = () => {
-    const eventSlides = [];
-    for (let i = 0; i < events.length; i += step) {
-      const eventCards = [];
-      for (let j = i; j < Math.min(i + step, events.length); j += 1) {
-        // prettier-ignore
-        eventCards.push(
-          <EventCard
-            key={`${events[j].header}-${j}`}
-            toggleModal={toggleModal}
-            index={j}
-            setEventIndex={setEventIndex}
-            header={events[j].header}
-            location={events[j].location}
-            sublocation={events[j].sublocation}
-            startDate={events[j].startDate}
-            endDate={events[j].endDate}
-            times={events[j].times}
-            image={events[j].image}
-            link={events[j].link}
-            tagType={events[j].tagType}
-          />,
-        );
-      }
-      eventSlides.push({
-        id: `event-slide-${i}`,
-        slide: <div className="event-carousel__slide">{eventCards}</div>,
-      });
-    }
-    return eventSlides;
-  };
   return (
     <>
-      <Carousel slides={createEventSlides()} />
+      <EventCard
+        key={`${formData.header}-${1}`}
+        toggleModal={toggleModal}
+        // index={j}
+        setEventIndex={() => {}}
+        header={formData.header}
+        location={formData.location}
+        sublocation={formData.sublocation}
+        startDate={formData.startDate}
+        endDate={formData.endDate}
+        times={formData.times}
+        image={formData.image}
+        link={formData.link}
+        tagType={formData.tagType}
+      />
       <EventModal
         isOpen={isOpen}
         toggleModal={toggleModal}
-        header={events[eventIndex].header}
-        location={events[eventIndex].location}
-        sublocation={events[eventIndex].sublocation}
-        startDate={events[eventIndex].startDate}
-        endDate={events[eventIndex].endDate}
-        tagType={events[eventIndex].tagType}
-        image={events[eventIndex].image}
-        times={events[eventIndex].times}
-        cohosts={events[eventIndex].cohosts}
-        link={events[eventIndex].link}
-        tagIcon={events[eventIndex].tagIcon}
+        header={formData.header}
+        location={formData.location}
+        sublocation={formData.sublocation}
+        startDate={formData.startDate}
+        endDate={formData.endDate}
+        tagType={formData.tagType}
+        image={formData.image}
+        times={formData.times}
+        cohosts={formData.cohosts}
+        link={formData.link}
+        tagIcon={formData.tagIcon}
       >
-        {events[eventIndex].body.split('\n').map((paragraph) => {
+        {formData.body.split('\n').map((paragraph) => {
           return (
             <p key={`event-modal-body-${paragraph.slice(0, 10)}`}>
               {paragraph}
@@ -101,4 +51,4 @@ const EventsCarousel = ({ formData }) => {
     </>
   );
 };
-export default EventsCarousel;
+export default EventsPreview;
