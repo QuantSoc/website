80aa7d58bc9f818a21129f52384295eeebeafa04
Shmu9 <sam.axford02@gmail.com>
2024-03-19 18:36:01 +1100

    Revert "refactor: align with eslint - import/prefer-default-export + no unused-vars."
    This reverts commit d9c4c4a3174b98df2839607f05e2faf212a5a36a.

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from 'firebase/analytics';
+import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,7 +20,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-const db = getFirestore();
-
-export default db;
+const analytics = getAnalytics(app);
+export const db = getFirestore();
