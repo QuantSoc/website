8c0b6f3ca2798a92fddaeac96652696c808d2d33
Cengiz Cimen <z5359299@ad.unsw.edu.au>
2024-04-18 10:23:44 +1000

    chore: updated some placeholder values

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -14,7 +14,8 @@ const ArticlesCard = ({
         <h4 className="article-card__heading">{heading}</h4>
         <h5 className="article-card__subheading">{subheading}</h5>
         <h6 className='article-card__author'>Written by {author}, {date}</h6>
-        <div className='article-card__sample'>{sampletext}</div>
+        <div className='article-card__sample'>{sampletext.substring(0,160)}...</div>
+        <button className='article-card__read-button'>READ MORE &gt;</button>
       </div>
     );
   };
--- src/routes/ArticlesPage/PreviouslySection/PreviouslySection.jsx
+++ src/routes/ArticlesPage/PreviouslySection/PreviouslySection.jsx
@@ -1,56 +1,58 @@
+import React from 'react';
 import './index.less';
-
 import ArticleCard from 'components/ArticleCard';
 
 const PreviouslySection = () => {
+
+  // Temporary placeholders until data drawn from firebase
+  const heading = "This is a heading for the article";
+  const subheading = "This is the subheading for an article. It expands on the heading ;P";
+  const author = 'Cengiz Cimen';
+  const date = '16th Jan 2023';
+  const sampletext = 'This is the first couple of lines from the article and gives us an idea of what it is about. Below should be a button sending us where we want to go...';
+
   return (
     <section className="past-articles">
-        <h1>Previously...</h1>
-        <div className='previously-flex'>
-            {/* NOTE::: until the database is updated the following are temporary placeholders simply to test the flex */}
-            <ArticleCard 
-            heading="This is a heading for the article" 
-            subheading="This is the subheading for an article. It expands on the heading ;P" 
-            author='Cengiz Cimen'
-            date='16th Jan 2023'
-            sampletext={'This is the first couple of lines from the article and gives us an idea of what it is about. Below should be a \
-            button sending us where we want to go...'}
-            />
-            <ArticleCard 
-            heading="This is a heading for the article" 
-            subheading="This is the subheading for an article. It expands on the heading ;P" 
-            author='Cengiz Cimen'
-            date='16th Jan 2023'
-            sampletext={'This is the first couple of lines from the article and gives us an idea of what it is about. Below should be a \
-            button sending us where we want to go...'}
-            />
-            <ArticleCard 
-            heading="This is a heading for the article" 
-            subheading="This is the subheading for an article. It expands on the heading ;P" 
-            author='Cengiz Cimen'
-            date='16th Jan 2023'
-            sampletext={'This is the first couple of lines from the article and gives us an idea of what it is about. Below should be a \
-            button sending us where we want to go...'}
-            />
-            <ArticleCard 
-            heading="This is a heading for the article" 
-            subheading="This is the subheading for an article. It expands on the heading ;P" 
-            author='Cengiz Cimen'
-            date='16th Jan 2023'
-            sampletext={'This is the first couple of lines from the article and gives us an idea of what it is about. Below should be a \
-            button sending us where we want to go...'}
-            />
-            <ArticleCard 
-            heading="This is a heading for another article" 
-            subheading="This is the subheading for an article. But you already know this" 
-            author='Victor Tang'
-            date='16th Jan 2024'
-            sampletext={'You know I am a really cool guy, I AM! Cooler than most at least. This is a big problem with society \
-            I am like the jonkler you cannot fuck with my i will just laugh hahahaha (very cool)...'}
-            />
-        </div>
-        
+      <h1>Previously...</h1>
+      <div className='previously-flex'>
+        <ArticleCard
+          heading={heading}
+          subheading={subheading}
+          author={author}
+          date={date}
+          sampletext={sampletext}
+        />
+        <ArticleCard
+          heading={heading}
+          subheading={subheading}
+          author={author}
+          date={date}
+          sampletext={sampletext}
+        />
+        <ArticleCard
+          heading={heading}
+          subheading={subheading}
+          author={author}
+          date={date}
+          sampletext={sampletext}
+        />
+        <ArticleCard
+          heading={heading}
+          subheading={subheading}
+          author={author}
+          date={date}
+          sampletext={sampletext}
+        />
+        <ArticleCard
+          heading={heading}
+          subheading={subheading}
+          author={author}
+          date={date}
+          sampletext={sampletext}
+        />
+      </div>
     </section>
   );
 };
+
 export default PreviouslySection;
