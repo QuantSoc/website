aa7ae2c6040a467e2c0e350911cd2b369f0a31da
Sam Trustrum <z5359860@ad.unsw.edu.au>
2024-06-21 20:23:12 +1000

    minor styling adjustment

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CookiesProvider } from 'react-cookie';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'styles/index.less';
-import { CookiesProvider } from 'react-cookie';
+
 
 ReactDOM.render(
   <React.StrictMode>
