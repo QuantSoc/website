b2602c42105a473082e7e14c789063113c46272a
Cengiz Cimen <z5359299@ad.unsw.edu.au>
2024-06-25 12:59:50 +1000

    feat: article page iframe added

--- src/components/ArticleCardLarge/ArticleCardLarge.jsx
+++ src/components/ArticleCardLarge/ArticleCardLarge.jsx
@@ -5,12 +5,12 @@ const ArticleCardLarge = ({
     subheading,
     image
 }) => {
-    // TODO: add more optional elements to the article card
+    // TODO: add more optional elements to the article card and automate the main page
     return (
       <div className="article-card-large__container">
-        <h4 className="article-card-large__heading">{heading}</h4>
-        <h5 className="article-card-large__subheading">{subheading}</h5>
-        <img src={image} alt="logo" className="article-card-large__image" />
+        {/* <h4 className="article-card-large__heading">{heading}</h4>
+        <h5 className="article-card-large__subheading">{subheading}</h5> */}
+        <iframe className='article-card-large__embed' src="https://preview.mailerlite.io/preview/829979/emails/122848449040221658" title="recent"></iframe>
       </div>
     );
   };
--- src/routes/ArticlesPage/RecentSection/RecentSection.jsx
+++ src/routes/ArticlesPage/RecentSection/RecentSection.jsx
@@ -9,12 +9,11 @@ import placeholder from 'assets/comingsoon.png'
 const RecentSection = () => {
   return (
     <section className="recent-articles">
-        <h1>Recent</h1>
+        <h1>Most Recently</h1>
 
         <ArticleCardLarge 
-        heading="Lorem Ipsum Dolor sit amet, consectetur adipiscing elit magna aliqua." 
-        subheading="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " 
-        image={placeholder}
+        heading="Welcome to our new QuantSoc Newsletter!" 
+        subheading="Let's have a look at what is going on this week. " 
         />
         
     </section>
