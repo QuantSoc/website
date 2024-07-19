1cf459228efdf333beaf4f2d8293a1bb51350fb0
Cengiz Cimen <z5359299@ad.unsw.edu.au>
2024-07-19 20:25:45 +1000

    chore: removed unused variables inside article card

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -1,26 +1,17 @@
 import './index.less';
 
-
 const ArticlesCard = ({
     heading,
-    subheading,
-    author,
     date,
-    sampletext,
     link,
-}) => {
-    // TODO: add more elements to the articles card
-    const handleClick = () => {
-      window.location.href = link;
-    };
-
+}) => {    
     return (
       <div className="article-card__container">
         <h4 className="article-card__heading">{heading}</h4>
         <h6>{date}</h6>
-        <button className='article-card__read-button' onClick={handleClick}>
+        <a href={link}><button className='article-card__read-button'>
           READ MORE &gt;
-        </button>      
+        </button></a>
         </div>
     );
   };
