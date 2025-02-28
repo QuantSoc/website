268de6edf25a48e31dea4f237e820f31394f458a
Cengiz Cimen <cengizcimen007@gmail.com>
2025-02-28 20:55:27 +1100

    fix: fixed the nmargins on the landing page

--- src/routes/LandingPage/LandingPage.jsx
+++ src/routes/LandingPage/LandingPage.jsx
@@ -6,27 +6,33 @@ import HeroSectionNew from './HeroSectionNew/HeroSectionNew';
 import SponsorshipSection from '../../components/SponsorshipSection';
 import AboutUsPage from 'routes/AboutUsPage';
 
+import './index.less';
+
+
 const LandingPage = () => {
   return (
     <div className="page">
       <HeroSectionNew />
-      <AboutUsPage />
-      <ResourceOutline
-        outlineTitle="Mock Trading Game"
-        index={0}
-        imgSrc={mockTradingScreenshot}
-        redirectLink="https://mtg.quantsoc.org"
-        redirectBtnText="Play Now"
-      >
-        <p>
-          Dive into the fast-paced world of trading, hone your investment
-          strategies, and compete with fellow traders in a mock environment.
-          <br/><br/>
-        Learn, compete and have fun in QuantSoc&apos;s very ownMock Trading Game!</p>
-      </ResourceOutline>
-      <EventsSection />
-      <ResourcesSection />
-      <SponsorshipSection />
+      <div className='main-body'>
+        <AboutUsPage />
+        <ResourceOutline
+          outlineTitle="Mock Trading Game"
+          index={0}
+          imgSrc={mockTradingScreenshot}
+          redirectLink="https://mtg.quantsoc.org"
+          redirectBtnText="Play Now"
+        >
+          <p>
+            Dive into the fast-paced world of trading, hone your investment
+            strategies, and compete with fellow traders in a mock environment.
+            <br/><br/>
+          Learn, compete and have fun in QuantSoc&apos;s very ownMock Trading Game!</p>
+        </ResourceOutline>
+        <EventsSection />
+        <ResourcesSection />
+        <SponsorshipSection />
+      </div>
+     
     </div>
   );
 };
