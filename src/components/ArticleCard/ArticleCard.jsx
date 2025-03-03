d2603f4f16be1e64efda8719b7747729fd6d317c
Cengiz Cimen <cengizcimen007@gmail.com>
2025-03-03 21:02:19 +1100

    chore: updated the news previously cards to share the same read more indicator as the events cards

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -1,19 +1,19 @@
 import './index.less';
+import { BiExpandVertical } from 'react-icons/bi';
 
-const ArticlesCard = ({
-    heading,
-    date,
-    link,
-}) => {    
-    return (
-      <div className="article-card__container">
-        <h4 className="article-card__heading">{heading}</h4>
-        <h6>{date}</h6>
-        <a href={link}><button className='article-card__read-button'>
-          READ MORE &gt;
-        </button></a>
+const ArticlesCard = ({ heading, date, link }) => {
+  return (
+    <div className="article-card__container">
+      <h4 className="article-card__heading">{heading}</h4>
+      <h6>{date}</h6>
+      <a href={link} style={{ textDecoration: 'none'}}>
+        <div className="event-card__button">
+          <BiExpandVertical />
+          <button type="button">Read More</button>
         </div>
-    );
-  };
-  export default ArticlesCard;
-  
+      </a>
+    </div>
+  );
+};
+
+export default ArticlesCard;
