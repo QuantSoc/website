2a0c6a341da41d4a5df0043fa854debdbec2e9e4
Toby Wang <z5590208@ad.unsw.edu.au>
2025-06-09 21:22:51 +1000

    feat: add place for gif in instruction carousel

--- src/routes/PentominoesPage/PentominoesHomePage.jsx
+++ src/routes/PentominoesPage/PentominoesHomePage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import './index.less';
-import Cookies from "js-cookie";
+import Cookies from 'js-cookie';
 import useModal from 'hooks/useModal';
 import Modal from 'components/Modal';
 import Carousel from 'components/Carousel/Carousel';
@@ -14,52 +14,62 @@ const Pentominoes = () => {
   }
 
   const instructionList = [
-    "You'll be given 5 different pentomino pieces. Each piece takes up exactly 5 tiles and can have various shapes and orientations.",
-    "Click or drag a piece to move it onto the 5×5 board. Pieces can be moved around freely until placed.",
-    "Select a piece and click the Rotate button to rotate it 90° clockwise. You can rotate multiple times to get the desired orientation.",
-    "Click the Flip button to flip the selected piece horizontally. Use both flip and rotate to explore all orientations.",
-    "Once a piece is in the correct spot and orientation, drop it onto the board. Pieces will snap into place if they fit.",
-    "You can remove or reposition pieces if needed. A reset button is provided to remove all progress.",
-    "Try fill the 5x5 grid as fast as possible and make it onto the leaderboard!"
+    'You\'ll be given 5 different pentomino pieces. Each piece takes up exactly 5 tiles and can have various shapes and orientations.',
+    'Click or drag a piece to move it onto the 5×5 board. Pieces can be moved around freely until placed.',
+    'Select a piece and click the Rotate button to rotate it 90° clockwise. You can rotate multiple times to get the desired orientation.',
+    'Click the Flip button to flip the selected piece horizontally. Use both flip and rotate to explore all orientations.',
+    'Once a piece is in the correct spot and orientation, drop it onto the board. Pieces will snap into place if they fit.',
+    'You can remove or reposition pieces if needed. A reset button is provided to remove all progress.',
+    'Try fill the 5x5 grid as fast as possible and make it onto the leaderboard!',
   ];
   const resultInstructions = [];
-  for (let i = 0; i < instructionList.length; i++) {
+  for (let i = 0; i < instructionList.length; i += 1) {
     resultInstructions.push({
       id: i,
-      slide: instructionList[i]
-    })
+      slide: 
+        <>
+          <div className="instructionCard__container">
+            {instructionList[i]}
+            <p>
+              <img 
+                alt="GIF here"
+                src="some.gif"
+                width="fit-content"
+              />
+            </p>
+            
+          </div>       
+        </>,
+    });
   }
-  
+
   return (
-    <div className='main-body'>
-      <div id='title'>Pentominoes</div>
-      { !active? (
+    <div className="main-body">
+      <div id="title">Pentominoes</div>
+      { !active ? (
         <>
           <div id="modalContainer">
-            <Modal 
+            <Modal
               isOpen={isOpen}
               toggleModal={toggleModal}
               header="How to play"
+              style={{justifyContent: "center", display: "flex"}}
             >
-              <Carousel slides={resultInstructions}></Carousel>       
+                <Carousel slides={resultInstructions} style={{justifySelf: "center"}}/>              
             </Modal>
           </div>
-          <div className='centreButtonContainer' style={{ flexDirection: 'column' }}>
-            <button className='button' onClick={toggleModal} type="button"> 
+          <div className="centreButtonContainer">
+            <button className="button" onClick={toggleModal} type="button">
               How to play
             </button>
-            <button className='button' onClick={startGame} autoFocus type="button"> 
+            <button className="button" onClick={startGame} type="button">
               Start
             </button>
           </div>
         </>
       ) : (
         <h1>WE ARE NOW PLAYING THE GAME</h1>
-      )
-      
-      
-      }
-      
+      )}
     </div>
   );
 };
